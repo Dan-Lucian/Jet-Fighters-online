f6d2743cc4e6f3f75050f887c97f575da2f4d776
Dan-Lucian <danugaina1403@gmail.com>
2021-10-08 18:16:16 +0300

    Eslint + prettier setup

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,9 +1,11 @@
 // import builing functions here
 import { ws } from './setup-websocket.js';
-ws.setupWsEvents();
-
 import { init } from './init-game.js';
+import { setupCanvasEvents } from './setup-canvas-events.js';
+
+ws.setupWsEvents();
 init();
 
-import { setupCanvasEvents } from './setup-canvas-events.js';
 setupCanvasEvents();
+
+let x;
